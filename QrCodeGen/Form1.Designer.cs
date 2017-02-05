c10dc7e1b2fe6965cd7ef3be6d040a24183a4528
jbk77 <josephbk.117@hotmail.com>
2017-02-05 13:12:57 +0530

    Add project files.

---
+++ QrCodeGen/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace QrCodeGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 275);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 429);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 540);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(275, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Back Ground Colour";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(275, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Fore Ground Colour";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.comboBox1.DisplayMember = "1";
+            this.comboBox1.DropDownHeight = 150;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 20;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Google Play Listing",
+            "Text",
+            "URL"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 395);
+            this.comboBox1.MaxDropDownItems = 3;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(275, 28);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.ValueMember = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(147, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0/120";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 85);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Custom Logo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 580);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Text = "Qr Code Generator";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ QrCodeGen/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zen.Barcode;
+using System.Threading;
+using MetroFramework.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+
+//TODO : Save it to differnt sizes
+namespace QrCodeGen
+{
+    public partial class Form1 : MetroForm
+    {
+        Bitmap bmp;
+        Color foreGround;
+        Color backGround;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                currentBGcol = Color.White;
+                currentFOcol = Color.Black;
+                CodeQrBarcodeDraw qrCode = BarcodeDrawFactory.CodeQr;
+                switch(comboBox1.SelectedIndex)
+                {
+                    case 1 : pictureBox1.Image = qrCode.Draw(textBox1.Text, 50, 6);
+                        break;
+                    case 0 : pictureBox1.Image = qrCode.Draw("https://play.google.com/store/apps/details?id=" + textBox1.Text, 50, 6);
+                        break;
+                    case 2 : pictureBox1.Image = qrCode.Draw("http://" + textBox1.Text, 50, 6);
+                        break;
+                }
+            }        
+        }
+
+        public static Color currentBGcol = Color.White;
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                backGround = cd.Color;
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Color colourToCheck = Color.White;                
+                colourToCheck = backGround;                
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        if (isColourSame(bmp.GetPixel(i, j),currentBGcol))
+                        {
+                            bmp.SetPixel(i, j, cd.Color);
+                            pictureBox1.Image = bmp;
+                        }
+                    }
+                }
+                currentBGcol = colourToCheck;
+                if (colourSimilarity(currentBGcol, currentFOcol) >= 80f)                    
+                    MetroFramework.MetroMessageBox.Show(this, "Colours Are Too Similar to be distinguished", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error,140);
+            }
+            
+        }
+
+        private Bitmap imageOuterEdge(Bitmap img,int size)
+        {
+            Bitmap bmp = new Bitmap(img.Width, img.Height, PixelFormat.Format32bppArgb);
+            //bmp.MakeTransparent();
+            Color transparent = Color.FromArgb(0, 0, 0, 0);
+            for(int i=0;i<img.Width;i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    if (!isColourSame(img.GetPixel(i, j), transparent))
+                    {
+
+                        //for (int k = 0; k < 40; k++)
+                        //{
+                        //    bmp.SetPixel(i + k, j + k, Color.White);
+                        //}
+
+                        //for (int k = 0; k < 40; k++)
+                        //{
+                        //    if(i-k >= 0 && j-k >= 0)
+                        //        bmp.SetPixel(i - k, j - k, Color.White);
+                        //}
+                        bmp.SetPixel(i, j, Color.White);
+                    }                          
+                                   
+                    }
+            }
+            return bmp;
+        }
+
+        private bool isColourSame(Color a , Color b)
+        {
+            if (a.R == b.R && a.G == b.G && a.B == b.B && a.A == b.A)
+                return true;
+            return false;
+        }
+
+        public static Color currentFOcol = Color.Black;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                foreGround = cd.Color;
+                Bitmap bmp = new Bitmap(pictureBox1.Image);
+                Color colourToCheck = Color.Black;
+                colourToCheck = foreGround;
+                for (int i = 0; i < bmp.Width; i++)
+                {
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        if (isColourSame(bmp.GetPixel(i, j), currentFOcol))
+                        {
+                            bmp.SetPixel(i, j, cd.Color);
+                            pictureBox1.Image = bmp;
+                        }
+                    }
+                }
+                currentFOcol = colourToCheck;
+                if (colourSimilarity(currentBGcol, currentFOcol) >= 80f)
+                    MetroFramework.MetroMessageBox.Show(this, "Colours Are Too Similar to be distinguished", "Error", MessageBoxButtons.OK, 140);
+            }
+        }
+        
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Color a = Color.FromArgb(255, 255, 255);
+            Color b = Color.FromArgb(0,0,0);
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int textLength = textBox1.Text.Length;
+            if (textLength <= 120)
+            {                
+                label1.Text = "" + textLength + "/120";                
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text.Substring(0, 119);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap tempImg;
+            SaveFileDialog sfd = new SaveFileDialog();
+            Rectangle rect = new Rectangle(0, 0, 270, 270);
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                tempImg = new Bitmap(pictureBox1.Image);
+                Bitmap mp = tempImg.Clone(rect, tempImg.PixelFormat);
+                mp.Save(sfd.FileName,System.Drawing.Imaging.ImageFormat.Jpeg);
+                tempImg.Dispose();
+                mp.Dispose();
+            }
+            
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            Image img = new Bitmap(pictureBox1.Image.Width, pictureBox1.Image.Height);
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {               
+                Bitmap baseImage;
+                Bitmap overlayImage;
+                baseImage = (Bitmap)pictureBox1.Image;
+                overlayImage = (Bitmap)Image.FromFile(ofd.FileName);
+                var finalImage = new Bitmap(baseImage.Width, baseImage.Height, PixelFormat.Format32bppArgb);
+                var graphics = Graphics.FromImage(finalImage);
+                graphics.CompositingMode = CompositingMode.SourceOver;
+                float overlayPosX = baseImage.Width / 2 - 40f;
+                float overlayPosY = baseImage.Height / 2 - 40f;
+                graphics.DrawImage(baseImage, 0, 0);
+                Brush b;
+                var br = new SolidBrush(backGround);
+                //graphics.FillRectangle(br, overlayPosX, overlayPosY, 60f, 60f);     
+                graphics.DrawImage(imageOuterEdge(overlayImage, 2), overlayPosX, overlayPosY,80f,80f);         
+                graphics.DrawImage(overlayImage, baseImage.Width/2-30f, baseImage.Height/2-30f, 60f, 60f);                
+                pictureBox1.Image = finalImage;
+            }
+            
+        }
+        //TODO : Fix this similarity check 
+        private float colourSimilarity(Color a,Color b)
+        {
+            return ((1f - (Math.Abs((float)(a.R + a.G + a.B) - (float)(b.R + b.G + b.B)) / (6f * 255f)))*100f);
+        }
+    }
+}
